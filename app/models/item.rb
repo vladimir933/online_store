2d21d0260a928b6c094bcb19f88e4dec5b848d40
Vladimir Ryabukhin <vovan.r.98@mail.ru>
2022-01-04 19:36:50 +0700

    add css styles for all pages and fix some bugs

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
     validates :name, length: { in: 2..50, message: 'Недопустимое количество символов в названии товара' }
     validates :kind, presence: { message: 'Не указана категория товара' }
     validates :delivery_time, presence: { message: 'Неверное время доставки товара' }
-    validates :description, length: { in: 5..200, message: 'Слишком маленькое описание товара' }
+    validates :description, length: { in: 5..200, message: 'Недопустимое описание товара, количество символов 5..200' }
     validates :price, length: { in: 0..999_999_999, message: 'Недопустимая цена товара' }
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+50.times do
+  item = {
+    :name => (2..rand(50)).map { ('а'..'я').to_a[rand(26)] }.join,
+    :user_id => 1,
+    :kind => ['Для дома', 'Для сада', 'Для животных'][rand(0..2)],
+    :description => (5..rand(200)).map { ('a'..'z').to_a[rand(26)] }.join,
+    :price => rand(1.0..999_999_998.99),
+    :delivery_time => rand(50)
+  }
+  
+  img = ['diman.png', 'klushkich.jpg', 'kostyan.png', '123.png', 'maksech.png', 'vadya.png', 'vit\'ka.png']
+  
+  items = Item.create(item)
+  items.avatar.attach(io: File.open(Rails.root.join("/home/vladimir/Изображения/#{img}")), filename: img)
+end
